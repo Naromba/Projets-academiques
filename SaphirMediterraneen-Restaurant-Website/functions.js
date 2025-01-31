94188be1dbc3c14fe7392ffe1016b23162db86f6
Naromba <naromba.conde@umontreal.ca>
2025-01-31 13:53:36 -0500

    Add files via upload
    Développement complet du site web interactif du restaurant The Blue Whale en HTML, CSS et JavaScript avec menu dynamique et réservation en ligne

---
+++ SaphirMediterraneen-Restaurant-Website/functions.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('menuButton').addEventListener('click', function() {
+        window.location.href = 'menu.html'
+    });
+    
+    document.getElementById('reserveButton').addEventListener('click', function(){
+        window.location.href= 'booking.html'
+    });
+
+});
+
+function popUp(){
+    alert("votre message a été envoyé!");
+}
+
+
+
+    
+
+
